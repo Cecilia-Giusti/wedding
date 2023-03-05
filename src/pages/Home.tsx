7ac5724e2e7eace169799132b38ea353b5d2aa9e
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-03-05 16:27:54 +0100

    Add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <main className='home'>
+      <div className='home-img'>
+        <img src='./assets/images/nom.png' alt='alexandre et cecilia' />
+      </div>
+      <div className='home-content'>
+        <p className='home-date'>
+          VENDREDI VINGT-ET-UN JUILLET | DEUX MILLE VINGT-TROIS
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
