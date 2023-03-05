a7cf134d7bca78884bf92d40ba32b527228d8a15
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-03-05 16:10:53 +0100

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className='header'>
+      <nav className='header-nav'>
+        <ul className='header-list'>
+          <li className='header-item'>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive
+                  ? "header-item-link active-accueil"
+                  : "header-item-link"
+              }>
+              Accueil
+            </NavLink>
+          </li>
+          <li className='header-item'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "header-item-link active-history"
+                  : "header-item-link"
+              }
+              to={"/notre-histoire"}>
+              Notre Histoire
+            </NavLink>
+          </li>
+          <li className='header-item'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "header-item-link active-organisation"
+                  : "header-item-link"
+              }
+              to={"/organisation"}>
+              Organisation
+            </NavLink>
+          </li>
+          <li className='header-item'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "header-item-link active-more"
+                  : "header-item-list-link"
+              }
+              to={"/les-plus"}>
+              Les plus
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
